bceeeadecd5065df41f9a88abde7a373bd3dd821
Amin Bm <medaminbenmbarek@gmail.com>
2020-01-13 15:52:41 +0100

    cleaning app deployment

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-docker build -t medamin2019/fib-client:latest -t medamin2019/fib-client:$SHA -f ./frontend/Dockerfile ./frontend
-docker build -t medamin2019/fib-server:latest -t medamin2019/fib-server:$SHA -f ./express/Dockerfile ./express
-docker build -t medamin2019/fib-worker:latest -t medamin2019/fib-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t $DOCKER_USERNAME/fib-client:latest -t $DOCKER_USERNAME/fib-client:$SHA -f ./frontend/Dockerfile ./frontend
+docker build -t $DOCKER_USERNAME/fib-server:latest -t $DOCKER_USERNAME/fib-server:$SHA -f ./express/Dockerfile ./express
+docker build -t $DOCKER_USERNAME/fib-worker:latest -t $DOCKER_USERNAME/fib-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push medamin2019/fib-client:latest
-docker push medamin2019/fib-server:latest
-docker push medamin2019/fib-worker:latest
+docker push $DOCKER_USERNAME/fib-client:latest
+docker push $DOCKER_USERNAME/fib-server:latest
+docker push $DOCKER_USERNAME/fib-worker:latest
 
-docker push medamin2019/fib-client:$SHA
-docker push medamin2019/fib-server:$SHA
-docker push medamin2019/fib-worker:$SHA
+docker push $DOCKER_USERNAME/fib-client:$SHA
+docker push $DOCKER_USERNAME/fib-server:$SHA
+docker push $DOCKER_USERNAME/fib-worker:$SHA
 
 
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=medamin2019/fib-server:$SHA
-kubectl set image deployments/client-deployment client=medamin2019/fib-client:$SHA
-kubectl set image deployments/worker-deployment worker=medamin2019/fib-worker:$SHA
+kubectl set image deployments/server-deployment server=$DOCKER_USERNAME/fib-server:$SHA
+kubectl set image deployments/client-deployment client=$DOCKER_USERNAME/fib-client:$SHA
+kubectl set image deployments/worker-deployment worker=$DOCKER_USERNAME/fib-worker:$SHA
